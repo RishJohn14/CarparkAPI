54b11905b89d6e5b2c9ab3ad9a93adff39732f62
Rishabh John <104306063+RishJohn14@users.noreply.github.com>
2023-02-02 23:35:05 +0800

    Add files via upload

---
+++ APIConnector.java
@@ -0,0 +1,117 @@
+ //package uk.ac.cam.cares.jps.agent.WeatherAPI;
+
+ import org.apache.http.client.HttpResponseException;
+ import org.apache.http.client.methods.CloseableHttpResponse;
+ import org.apache.http.client.methods.HttpGet;
+ import org.apache.http.impl.client.CloseableHttpClient;
+ import org.apache.http.impl.client.HttpClients;
+ import org.apache.http.util.EntityUtils;
+ import org.apache.logging.log4j.LogManager;
+ import org.apache.logging.log4j.Logger;
+ import org.json.JSONException;
+ import org.json.JSONObject;
+ import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
+ import java.time.LocalDateTime;
+ import java.time.format.DateTimeFormatter;
+ 
+ 
+ import java.io.*;
+ import java.util.Properties;
+ 
+ import javax.print.attribute.standard.JobHoldUntil;
+ 
+ 
+ 
+ public class APIConnector
+ {
+     private String API_URL = "http://datamall2.mytransport.sg/ltaodataservice/CarParkAvailabilityv2";
+     private String date;
+     
+     private static final String ERRORMSG = "Carpark data could not be retrieved";
+     private static final Logger LOG = LogManager.getLogger(APIAgentLauncher.class);
+   
+ 
+     //Standard Constructor to initialise the instance variables
+     public APIConnector(String URL, String d)
+     {
+         API_URL=URL;
+         date = d;
+     }
+     
+ 
+     //Constructor to initialise the variables according to the Properties file
+ 
+     public APIConnector(String filepath) throws IOException
+     {
+         loadAPIConfigs(filepath);
+     }      
+ 
+     // Obtains Weather data in JSON format containing key:value pairs
+ 
+     public JSONObject getReadings()
+     {
+         try{
+             return retrieveData();
+ 
+         }
+         catch(IOException e)
+         {
+             LOG.error(ERRORMSG);
+             throw new JPSRuntimeException(ERRORMSG,e);
+         }
+     }
+ 
+     private JSONObject retrieveData() throws IOException, JSONException
+     {  
+         String path = API_URL;
+ 
+         try ( CloseableHttpClient httpclient =  HttpClients.createDefault())
+         {
+             HttpGet readrequest = new HttpGet(path);
+             try ( CloseableHttpResponse response = httpclient.execute(readrequest))
+             {
+                 int status = response.getStatusLine().getStatusCode();
+ 
+                 if(status==200) 
+                 {
+                     return new JSONObject(EntityUtils.toString(response.getEntity()));
+ 
+                 }
+                 else
+                 {
+                     throw new HttpResponseException(status,"Data could not be retrieved due to a server error");
+                 }
+ 
+             }
+ 
+         }
+ 
+     }
+ 
+     private void loadAPIConfigs(String filepath) throws IOException
+     {
+         File file = new File(filepath);
+         if(!file.exists())
+         {
+             throw new FileNotFoundException("There was no file found in the path");
+         }
+         
+         try (InputStream input = new FileInputStream(file))
+         {
+             Properties prop = new Properties();
+             prop.load(input);
+ 
+             if(prop.containsKey("carpark.api_url"))
+             {
+                 this.API_URL = prop.getProperty("carpark.api_url");
+             }
+             else
+             {
+                 throw new IOException("The file is missing: \"carpark.api_url=<api_url>\"");
+             }
+             
+ 
+         }
+     }
+ 
+ }
---
+++ APIInputAgent.java
@@ -0,0 +1,395 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jooq.exception.DataAccessException;
+import uk.ac.cam.cares.jps.base.util.JSONKeyToIRIMapper;
+import uk.ac.cam.cares.jps.base.timeseries.TimeSeries;
+import uk.ac.cam.cares.jps.base.timeseries.TimeSeriesClient;
+import uk.ac.cam.cares.jps.base.timeseries.TimeSeriesSparql;
+
+
+import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.zip.GZIPInputStream;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class APIInputAgent
+{
+    public static final Logger Log = LogManager.getLogger(APIAgentLauncher.class);
+    private TimeSeriesClient<OffsetDateTime> tsclient;
+    private List<JSONKeyToIRIMapper> mappings;
+    public static final String generatedIRIPrefix = TimeSeriesSparql.ns_kb + "Carpark";
+    public static final String timeUnit = OffsetDateTime.class.getSimpleName();
+    public static final String timestampKey = "start";
+    //public static final String status = "qcstatus";
+    public static final ZoneOffset ZONE_OFFSET = ZoneOffset.UTC;
+ 
+
+
+    public APIInputAgent(String propertiesFile) throws IOException
+    {
+
+        try(InputStream input = new FileInputStream(propertiesFile))
+        {
+            Properties prop = new Properties();
+            prop.load(input);
+            String mappingFolder;
+
+            try
+            {
+                mappingFolder = System.getenv(prop.getProperty("Carpark.mappingfolder")); 
+                //to substitute for values i need;
+            }
+            catch(NullPointerException e)
+            {
+                throw new IOException("The key Carpark.mappingfolder cannot be found in the file");
+
+            }
+
+            if(mappingFolder==null)
+            {
+                throw new InvalidPropertiesFormatException("The properties file does not contain the key Carpark.mappingfolder with a path to the folder containing the required JSON key to IRI Mappings");
+            }
+
+            readmappings(mappingFolder);
+        }
+
+    }
+
+    public int getNumberofTimeSeries()
+    {
+        return mappings.size();
+    }
+
+    public void setTsClient(TimeSeriesClient<OffsetDateTime> tsclient)
+    {
+        this.tsclient = tsclient;
+    }
+
+    private void readmappings(String mappingfolder) throws IOException
+    {
+        mappings = new ArrayList<>();
+        File folder = new File(mappingfolder);
+        File[] mappingFiles = folder.listFiles();
+
+        if(mappingFiles==null)
+        {
+            throw new IOException("Folder does not exist: " + mappingfolder);
+        }
+        if(mappingFiles.length==0)
+        {
+            throw new IOException("No files in folder");
+
+        }
+        else
+        {
+            for( File mappingFile: mappingFiles)
+            {
+                JSONKeyToIRIMapper mapper = new JSONKeyToIRIMapper(APIInputAgent.generatedIRIPrefix, mappingFile.getAbsolutePath());
+                mappings.add(mapper);
+                mapper.saveToFile(mappingFile.getAbsolutePath());
+            }
+        }
+    }
+
+    public void initializeTimeSeriesIfNotExist()
+    {
+        for(JSONKeyToIRIMapper mapping:mappings)
+        {
+            List<String> iris = mapping.getAllIRIs();
+            if(!timeSeriesExist(iris))
+            {
+                List<Class<?>> classes = iris.stream().map(this::getClassFromJSONKey).collect(Collectors.toList());
+                // TO clarify later on Google.
+                
+                try
+                {
+                    tsclient.initTimeSeries(iris,classes,timeUnit);
+                    Log.info(String.format("Initialized time series with the following IRIs: %s", String.join(", ", iris)));
+                
+                }
+                catch(Exception e)
+                {
+                    throw new JPSRuntimeException("Could not instantiate TimeSeries");
+                }
+            }
+        }
+    }
+    private boolean timeSeriesExist(List<String> iris)
+    {
+        for (String iri:iris)
+        {
+            try
+            {
+                if(!tsclient.checkDataHasTimeSeries(iri))
+                {
+                    return false;
+                }
+            }
+            catch(DataAccessException e)
+            {
+                if (e.getMessage().contains("ERROR: relation \"dbTable\" does not exist")) 
+                {
+                    return false;
+                }
+                else 
+                {
+                    throw e;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void updateData(JSONObject carparkReadings) throws IllegalArgumentException
+    {
+        Map <String, List<?>> carparkReadingsMap = new HashMap<>();
+        try
+        {
+            carparkReadingsMap = jsonObjectToMap(carparkReadings);
+        }
+        catch (Exception e) 
+        {
+            throw new JPSRuntimeException (e.toString());
+        }
+
+
+        if(!carparkReadings.isEmpty())
+        {
+            List<TimeSeries<OffsetDateTime>> timeSeries;
+            try
+            {
+                timeSeries = convertReadingsToTimeSeries(carparkReadingsMap);
+            }
+            catch (NoSuchElementException e)    
+            {
+                throw new IllegalArgumentException("Readings cannot be converted to ProperTimeSeries",e);
+            }
+            for (TimeSeries<OffsetDateTime> ts : timeSeries) 
+            {
+                // Retrieve current maximum time to avoid duplicate entries (can be null if no data is in the database yet)
+                OffsetDateTime endDataTime;
+                try 
+                 {
+                	endDataTime= tsclient.getMaxTime(ts.getDataIRIs().get(0));
+                 } 
+                 catch (Exception e) 
+                 {
+                	throw new JPSRuntimeException("Could not get max time!");
+                  }
+                OffsetDateTime startCurrentTime = ts.getTimes().get(0);
+                // If there is already a maximum time
+                if (endDataTime != null) 
+                {
+                    // If the new data overlaps with existing timestamps, prune the new ones
+                    if (startCurrentTime.isBefore(endDataTime))
+                        ts = pruneTimeSeries(ts, endDataTime);
+                }
+                // Only update if there actually is data
+                if (!ts.getTimes().isEmpty()) 
+                {
+                	try 
+                    {
+                      tsclient.addTimeSeriesData(ts);
+                      Log.debug(String.format("Time series updated for following IRIs: %s", String.join(", ", ts.getDataIRIs())));
+                    }
+                    catch (Exception e)
+                    {
+                	   throw new JPSRuntimeException("Could not add timeseries!");
+                    } 
+                }
+            }
+        }
+        else 
+        {
+            throw new IllegalArgumentException("Readings can not be empty!");
+        }
+    }
+
+    private Map<String, List<?>> jsonObjectToMap(JSONObject readings) {
+
+        // First save the values as Object //
+        
+        JSONArray jsArr;
+        try {
+            jsArr = readings.getJSONArray("items");
+            for(int i=0; i<jsArr.length();i++) {
+                Map<String, List<?>> readingsMap = new HashMap<>();
+                JSONObject currentEntry = jsArr.getJSONObject(i);
+                Iterator<String> it = currentEntry.keys();
+                while(it.hasNext()) 
+                {
+                    String key = it.next();
+                    Object value = currentEntry.get(key);
+
+                    if(value == JSONObject.NULL)
+                    value="NA";
+
+                    if(!readingsMap.contains(key))
+                    readingsMap.put(key, new ArrayList<>());
+
+                    readingsMap.get(key).add(value);
+
+                    List<Object> valuesUntyped = readingsMap.get(key);
+                    List<?> valuesTyped;
+
+                    if(key.equals("AvailableLots"))
+                    {
+                        valuesTyped = valuesUntyped.stream().map(x -> ((Number) x).intValue()).collect(Collectors.toList());
+                    }
+                    else
+                    {
+                        valuesTyped = valuesUntyped.stream().map(Object::toString).collect(Collectors.toList());
+                    }
+
+                    readingsMap.put(key,valuesTyped);
+
+
+                    //Create Time series here for individual JSONObjects
+
+                }
+            }
+        } catch (Exception e) {
+            throw new JPSRuntimeException("Readings can not be empty!", e);
+        }   
+
+        // Convert the values to the proper datatype //
+        Map<String, List<?>> readingsMapTyped = new HashMap<>();
+       
+        return readingsMapTyped;
+
+    }
+
+
+    private List<TimeSeries<OffsetDateTime>> convertReadingsToTimeSeries(Map<String, List<?>> carparkReadings)
+    throws  NoSuchElementException 
+    {
+       // Extract the timestamps by mapping the private conversion method on the list items
+       // that are supposed to be string (toString() is necessary as the map contains lists of different types)
+
+       List<OffsetDateTime> carparkTimestamps = carparkReadings.get(APIInputAgent.timestampKey).stream().map(timestamp -> (convertStringToOffsetDateTime(timestamp.toString()))).collect(Collectors.toList());
+
+       // Construct a time series object for each mapping
+       List<TimeSeries<OffsetDateTime>> timeSeries = new ArrayList<>();
+       for (JSONKeyToIRIMapper mapping: mappings)
+      {
+          // Initialize the list of IRIs
+            List<String> iris = new ArrayList<>();
+           // Initialize the list of list of values
+            List<List<?>> values = new ArrayList<>();
+           for(String key: mapping.getAllJSONKeys()) 
+            {
+                // Add IRI
+               iris.add(mapping.getIRI(key));
+               if (carparkReadings.containsKey(key)) 
+                {
+                  values.add(carparkReadings.get(key));
+                }
+                else 
+                {
+                 throw new NoSuchElementException("The key " + key + " is not contained in the readings!");
+                }
+            }
+
+
+            
+
+          List<OffsetDateTime> times = carparkTimestamps;
+          // Create the time series object and add it to the list
+   
+         TimeSeries<OffsetDateTime> currentTimeSeries = new TimeSeries<>(times, iris, values);
+         timeSeries.add(currentTimeSeries);
+      }
+
+     return timeSeries;
+   }
+   private OffsetDateTime convertStringToOffsetDateTime(String timestamp)  
+   {
+     timestamp=timestamp.replace("+08:00","");
+
+     DateTimeFormatter dtf=DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+     LocalDateTime localTime=LocalDateTime.parse(timestamp,dtf);
+
+
+     // Then add the zone id
+     LocalDateTime now = LocalDateTime.now();
+     ZoneId zone = ZoneId.of("Asia/Singapore");
+     ZoneOffset zoneOffset = zone.getRules().getOffset(now);
+
+
+
+
+     return OffsetDateTime.of(localTime,zoneOffset);
+   }
+
+
+
+
+
+
+   private TimeSeries<OffsetDateTime> pruneTimeSeries(TimeSeries<OffsetDateTime> timeSeries, OffsetDateTime timeThreshold) 
+   {
+     // Find the index from which to start
+     List<OffsetDateTime> times = timeSeries.getTimes();
+     int index = 0;
+     while(index < times.size()) 
+     {
+        if (times.get(index).isAfter(timeThreshold)) 
+        {
+            break;
+        }
+        index++;
+     }
+     // Prune timestamps
+     List<OffsetDateTime> newTimes = new ArrayList<>();
+     // There are timestamps above the threshold
+     if (index != times.size()) 
+      {
+         // Prune the times
+         newTimes = new ArrayList<>(times.subList(index, times.size()));
+      }
+     // Prune data
+     List<List<?>> newValues = new ArrayList<>();
+     // Prune the values
+     for (String iri: timeSeries.getDataIRIs())
+       {
+         // There are timestamps above the threshold
+         if (index != times.size()) 
+          {
+             newValues.add(timeSeries.getValues(iri).subList(index, times.size()));
+          }
+         else 
+          {
+             newValues.add(new ArrayList<>());
+          }
+       }
+     return new TimeSeries<>(newTimes, timeSeries.getDataIRIs(), newValues); 
+    }
+
+
+
+
+
+    private Class<?> getClassFromJSONKey(String jsonKey) 
+    {
+         if (jsonKey.contains("relative_humiditylow") || jsonKey.contains("relative_humidityhigh") || jsonKey.contains("temperaturelow") || jsonKey.contains("temperaturehigh")||jsonKey.contains("windspeedlow")||jsonKey.contains("windspeedhigh"))
+         {
+             return Double.class;
+         }
+        else
+         {
+            return String.class;
+         }
+    }
+
+
+}
